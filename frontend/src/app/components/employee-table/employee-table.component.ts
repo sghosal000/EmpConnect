39985321f11d6ce1a34c6be1f81807c54d433e06
sghosal <sghosal@test.com>
2025-01-16 16:10:47 +0530

    search added, pagination working

--- frontend/src/app/components/employee-table/employee-table.component.ts
+++ frontend/src/app/components/employee-table/employee-table.component.ts
@@ -1,23 +1,49 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { EmployeeService } from '../../services/employee.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-table',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './employee-table.component.html',
   styleUrl: './employee-table.component.css'
 })
-export class EmployeeTableComponent {
+export class EmployeeTableComponent implements OnInit, OnChanges {
   @Input() employees: any = []
   @Input() loading = true
   @Input() error = ''
 
+  @Output() addEmployee = new EventEmitter<void>()
+
   currentSorting = ''
   ascending = true
+  searchStr = ''
+  filteredEmployees: any = []
+  
+  pageEmployees: any = []
+  itemsPerPage = 5
+  currentPage = 0
+  totalPages = 1
+
 
   constructor(private router: Router, private employeeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.filteredEmployees = [...this.employees]
+    this.updatePagination()
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.searchEmployees();
+    this.filteredEmployees = [...this.employees];
+    this.updatePagination()
+  }
+
+  openAddEmployeeModal() {
+    this.addEmployee.emit()
+  }
 
   editEmployee(empId: number) {
     this.router.navigate([`/edit/${empId}`])
@@ -31,6 +57,8 @@ export class EmployeeTableComponent {
           this.employeeService.getEmployees().subscribe({
             next: (data) => {
               this.employees = data
+              this.filteredEmployees = data
+              this.updatePagination()
             },
             error: (err) => {
               this.error = "Error fetching Employee data"
@@ -54,10 +82,43 @@ export class EmployeeTableComponent {
       this.ascending = true
     }
 
-    this.employees.sort((a: any, b: any) => {
-      if (a[field] < b[field]) return this.ascending ? -1 : 1;
-      if (a[field] > b[field]) return this.ascending ? 1 : -1;
+    this.filteredEmployees.sort((a: any, b: any) => {
+      const x = a[field] ?? ''
+      const y = b[field] ?? ''
+
+      if (!x && y) this.ascending ? -1 : 1;
+      if (x && !y) this.ascending ? 1 : -1;
+
+      // if (typeof x === 'string' && typeof y === 'string'){}
+
+      if (x < y) return this.ascending ? -1 : 1;
+      if (x > y) return this.ascending ? 1 : -1;
       return 0
     })
+
+    this.updatePagination()
+  }
+
+  searchEmployees() {
+    const searchStrLower = this.searchStr.toLocaleLowerCase()
+    this.filteredEmployees = this.employees.filter((employee: any) =>
+      employee.empId?.toString().toLocaleLowerCase().includes(searchStrLower) ||
+      employee.name?.toLocaleLowerCase().includes(searchStrLower)
+    )
+
+    this.updatePagination()
+  }
+
+  updatePagination() {
+    this.totalPages = Math.ceil(this.filteredEmployees.length / this.itemsPerPage)
+    this.pageEmployees = this.filteredEmployees.slice(
+      this.currentPage * this.itemsPerPage,
+      (this.currentPage + 1) * this.itemsPerPage
+    )
+  }
+
+  changePage(page: number) {
+    this.currentPage = page
+    this.updatePagination()
   }
 }
